d8c17d071e4b678e32fc486b68a1ce9844c8d884
Neloy Barman <neloycareer018@gmail.com>
2025-02-20 18:30:40 +0600

    Contact Info Handler Added

---
+++ ZIES_Intent_Handlers/intent_handlers/contact_info_hlr.py
@@ -0,0 +1,68 @@
+from helpers.generic import get_slot_category
+from helpers.lex_response import nextIntentWithResponseCard
+
+def handle_contact(event):
+
+    # Session Attributes
+    session_attributes = event["sessionAttributes"] if event["sessionAttributes"] is not None else {}
+
+    # Fetching slot value
+    contact = event['currentIntent']['slots']['Contact']
+    print(f"Contact Name: {contact}")
+    
+    # Options
+    options = [
+        {
+            'text': "Main Menu",
+            'value': "Main Menu"
+        },
+        {
+            'text': "About Us",
+            'value': "About Us"
+        }
+    ]
+
+    # Slot Type values
+    email = ["email", "official email", "email address", "official email id", "mail address"]
+
+    # List creation with slot type values list and Category
+    contacts = [
+        (email, "Email")
+    ]
+
+    # All the contacts info
+    contacts_info = {
+        "Email": "The official email for ZIES is <strong>zies2023@gmail.com</strong>."
+    }
+
+    # Response Message 
+    if contact is None:
+        message = """
+            Call us at: <strong>+91-96210-47786</strong> Or <strong>+91-97938-85136</strong>
+            ~Email us at:</strong> <strong>zies2023@gmail.com</strong>
+            ~Visit our instagram handle<strong><a href="https://www.instagram.com/zies_scholarbirds?igsh=d3pmM2VsYzlxMDZm" target="_blank">here</a></strong>
+        """
+    else:
+        # Fetch Category
+        contact_cat = get_slot_category(
+            slot_lists  = contacts, 
+            slot = contact.lower()
+        )
+        print(f"Categorized partner: {contact_cat}")
+
+        # Fetch contacts Info
+        message = contacts_info[contact_cat]
+
+    title = None
+    subTitle = None
+    imageUrl = None
+
+    return nextIntentWithResponseCard(
+        session_attributes,
+        message, 
+        title,
+        subTitle,
+        imageUrl,
+        options
+    )
+
--- ZIES_Intent_Handlers/lambda_function.py
+++ ZIES_Intent_Handlers/lambda_function.py
@@ -1,6 +1,7 @@
 import json
 
 from intent_handlers.sponsor_info_hlr import handle_sponsor
+from intent_handlers.contact_info_hlr import handle_contact
 from intent_handlers.location_timing_info_hlr import handle_location_timing
 from intent_handlers.guest_of_honour_info_hlr import handle_guest_of_honour
 
@@ -14,6 +15,7 @@ def lambda_handler(event, context):
         "Guest_Of_Honour_Info_ZIES" : handle_guest_of_honour,
         "Sponsor_Partner_Info_ZIES": handle_sponsor,
         "Location_Timings_ZIES": handle_location_timing,
+        "Contact_Info_ZIES": handle_contact
     }
 
     return intent_to_handlers[intent_name](event)
